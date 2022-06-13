b0940ab91044dbd8a99d34d4117480c361433e86
Lin_Kangjing <linkangjing@foxmai.com>
2022-06-13 21:34:49 +0800

    自定义loader

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@
  * @FilePath: \webpackPractice\src\index.js
  * @Date: 2022-06-11 16:25:22
  * @LastEditors: Lin_kangjing
- * @LastEditTime: 2022-06-12 19:56:47
+ * @LastEditTime: 2022-06-13 20:29:35
  * @author: Lin_kangjing
  */
-import {VueElement} from 'vue'
-import {createRouter} from 'vue-router'
 import write from './test'
 import './index.css'
 write()
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,57 @@
-
+/*
+ * @Description:
+ * @FilePath: \webpackPractice\webpack.config.js
+ * @Date: 2022-06-11 16:24:41
+ * @LastEditors: Lin_kangjing
+ * @LastEditTime: 2022-06-13 21:02:23
+ * @author: Lin_kangjing
+ */
+const path = require('path');
 module.exports = {
   entry: {
-    bundle:"./src/index.js",
-    vendor:['vue','vue-router']
+    bundle: "./src/index.js",
+    vendor: ["vue", "vue-router"],
   },
   output: {
     filename: "[name].js",
   },
   devServer: {
     static: "./",
-    open:true
+    open: true,
   },
   mode: "development",
-  module:{
-    rules:[{
-      test:/\.css$/,
-      use:['style-loader','css-loader']
-    }]
-  }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    modules: false,
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            loader: path.resolve("force-strict-loader/index.js"),
+            options:{
+              sourceMap: true,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
